902986e172b272ce79c81bbabd56dc1c55726b1b
rahulprops <rahulhack912@gmail.com>
2025-04-07 16:31:39 +0530

    UserSchema create

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
 import express from 'express'
 import 'dotenv/config.js'
 import db from './config/db.js';
+import cookieParser from 'cookie-parser';
 
 
 const app=express()
 const port=process.env.PORT || 3000;
+
+//! moiddlewares
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
 
 //! server start
 app.listen(port,()=>{
---
+++ backend/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose, { model, Schema } from "mongoose";
+
+
+const userSchema= new Schema({
+    username:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    isAdmin:{
+        type:Boolean,
+        required:true,
+        default:false
+    }
+
+},{timestamps: true})
+
+const User=model("User",userSchema)
+export default User;
